75f0d3e36bfc981d695c0de36be71c5eb72541b0
anokthomas <135610860+anokthomas@users.noreply.github.com>
2023-06-05 19:12:31 +0530

    Add files via upload

---
+++ ch4drill10.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <vector>
+int main() {
+    double num1, smallest,largest;
+    bool firstinput = true;
+    std :: string unit; //This is based on my intrepretation of the problem , solutions may vary .I humbly apologise for any mistakes
+    double cm = 1;
+    double metres = 100;
+    double inches = 2.54;
+    double feet = 12*2.54;
+    double sum = 0;
+    double values = 0;
+    std :: vector <int> numbers;
+    while (true) {
+        std :: cout << "Enter a value or press | to terminate" << std:: endl;
+        std :: cout << " After giving value,Enter an unit(cm,in,m,ft)" << std::endl;
+        if (!(std ::cin >> num1)|| num1 == '|'||!(std::cin >> unit)){
+            break;
+        }
+        numbers.push_back(num1);
+        if (firstinput == true){
+            if(unit == "cm"){
+                smallest = num1;
+                largest = num1;
+                sum += num1;
+                values += 1; 
+            }
+            else if (unit == "m")
+            {
+                smallest = num1*metres;
+                largest = num1*metres;
+                sum += num1*metres;
+                values += 1;
+            }
+            else if (unit == "in"){
+                smallest = num1*inches;
+                largest = num1*inches;
+                sum += num1*metres;
+                values += 1;
+            }
+            else if(unit == "ft"){
+                smallest = num1*feet;
+                largest = num1*feet;
+                sum += num1*metres;
+                values += 1;
+            }
+            else{
+                std :: cout << "Wrong unit value" << std:: endl;
+            }
+            firstinput = false;  
+        }
+        else{
+            if ((smallest > num1) && unit == "cm"){
+                smallest = num1;
+                sum += num1;
+                values += 1;
+            }
+            else if ((largest < num1) && unit == "cm"){
+                largest = num1;
+                sum += num1;
+                values += 1;
+            }
+            else if ((smallest > num1) && unit == "m"){
+                smallest = num1*metres;
+                sum += num1*metres;
+                values += 1;
+            }
+            else if ((largest < num1) && unit == "m"){
+                largest = num1*metres;
+                sum += num1*metres;
+                values += 1;
+            }
+            else if ((smallest > num1) && unit == "in"){
+                smallest = num1*inches;
+                sum += num1*inches;
+                values += 1;
+            }
+            else if ((largest < num1) && unit == "in"){
+                largest = inches;
+                sum += num1*inches;
+                values += 1;
+            }
+            else if ((smallest > num1) && unit == "ft"){
+                smallest = num1*feet;
+                sum += num1*feet;
+                values += 1;
+            }
+            else if ((largest < num1) && unit == "ft"){
+                largest = num1*feet;
+                sum += num1*feet;
+                values += 1;
+            }
+            else{
+                if(unit == "cm"){
+                sum += num1;
+                values += 1;
+            }
+            else if (unit == "m")
+            {
+                sum += num1*metres;
+                values += 1;
+            
+            }
+            else if (unit == "in"){
+                sum += num1*metres;
+                values += 1;
+            }
+            else if(unit == "ft"){
+                sum += num1*metres;
+                values += 1;
+            }
+            else{
+                std :: cout << "Wrong unit value" << std::endl;
+            }     
+            } 
+            std :: cout << "The largest value so far(values in cm)\t" << largest << std::endl;
+            std :: cout << "The smallest value so far(values in cm)\t" << smallest << std::endl;
+            std :: cout << "The sum is:\t" << sum << std::endl;
+            std :: cout << "The number of values is :\t" << values << std::endl;
+
+        }
+    }
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+    std :: cout << "Program terminated.\n" << std::endl;
+    return 0;
+}
+
+
+
+
+
